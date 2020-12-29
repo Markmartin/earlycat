01427470428258883e60ed498a0026f62da0b465
古月 <944565923@qq.com>
2020-12-29 16:13:12 +0800

    更新开发进度

--- config/variable.js
+++ config/variable.js
@@ -1,4 +1,4 @@
 // 生产环境
-export const WxApiRoot = 'https://wx.earlycat.cn/wx/'
+// export const WxApiRoot = 'https://wx.earlycat.cn/wx/'
 // 测试环境
-// export const WxApiRoot = 'https://wxtest.earlycat.cn/wx/'
+export const WxApiRoot = 'https://wxtest.earlycat.cn/wx/'
